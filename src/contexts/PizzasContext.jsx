c09bba3ad7417c629a4d548f57adb19db4427220
Alejandro Gómez Montoya <alejongo@Alejandros-MacBook-Pro.local>
2025-04-08 18:24:38 -0500

    :bug: FIX FETCH URL: Fix URL to fetch pizzas

--- src/contexts/PizzasContext.jsx
+++ src/contexts/PizzasContext.jsx
@@ -5,7 +5,7 @@ export const PizzasContext = createContext();
 
 export const PizzasProvider = ({ children }) => {
   const { data, isLoading, hasError, error } = useFetchPizza(
-    "http://localhost:5001/api/pizzas"
+    "http://localhost:5000/api/pizzas"
   );
 
   return (
